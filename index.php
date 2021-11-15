d0da34bf3577c0d68fcf9e3337eba20332c60323
andreina <andreinaarias5@gmail.com>
2021-11-15 16:49:43 -0300

    subiendo primeros archivos

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+echo "hola mundo";
+echo "<br>";
+var_dump(10);
+
+// comentario una línea
+
+/* comentario 
+multilinea*/
+
+// variables : guarda espacio en memoria, guarda datos
+echo "<br>";
+$nombre= "Andreina";
+$apellido="Arias";
+$edad=35;
+$hobbie="La música";
+$editor_codpreferido="Visual Studio Code";
+$sistema_operativo="Windows 10";
+
+
+echo "Nombre:".$nombre;
+echo "<br>";
+echo  "Apellido:".$apellido;
+echo "<br>";
+echo "Edad:".$edad;
+echo "<br>";
+echo "Hobbie:".$hobbie;
+echo "<br>";
+echo "Editor_codpreferido:".$editor_codpreferido;
+echo "<br>";
+echo "Sistema_operativo:".$sistema_operativo;
+
+
+?>
